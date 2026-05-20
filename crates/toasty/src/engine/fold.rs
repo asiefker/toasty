4cacc09977a8314fb4cfda720a66e273bdfabb85
Adam Siefker <siefker@gmail.com>
2026-05-20 21:32:10 +0000

    Support 'include' for item collections
    Add an stmt::Expr::IsModel to model the sort key prefix for item
    collections.

--- crates/toasty/src/engine/fold.rs
+++ crates/toasty/src/engine/fold.rs
@@ -82,6 +82,7 @@ fn fold_one(i: &mut Expr) -> Option<Expr> {
         | Expr::Exists(_)
         | Expr::Func(_)
         | Expr::Ident(_)
+        | Expr::IsModel(_)
         | Expr::InSubquery(_)
         | Expr::IsVariant(_)
         | Expr::Length(_)
